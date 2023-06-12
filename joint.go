ad4a53f1b9a6f9c5d5870a22b371b7dfb747bfa2
Brent Farris <brentfarris@outlook.com>
2023-06-12 08:18:31 -0500

    Removing deprecated calls (panic for now)

--- joint.go
+++ joint.go
@@ -598,7 +598,9 @@ func (j Hinge2Joint) Anchor2() Vector3 {
 
 // SetAxis1 sets the first axis.
 func (j Hinge2Joint) SetAxis1(axis Vector3) {
-	C.dJointSetHinge2Axis1(j.c(), C.dReal(axis[0]), C.dReal(axis[1]), C.dReal(axis[2]))
+	// TODO:  Fix this
+	panic("deprecated")
+	//C.dJointSetHinge2Axis1(j.c(), C.dReal(axis[0]), C.dReal(axis[1]), C.dReal(axis[2]))
 }
 
 // Axis1 returns the first axis.
@@ -610,7 +612,9 @@ func (j Hinge2Joint) Axis1() Vector3 {
 
 // SetAxis2 sets the second axis.
 func (j Hinge2Joint) SetAxis2(axis Vector3) {
-	C.dJointSetHinge2Axis2(j.c(), C.dReal(axis[0]), C.dReal(axis[1]), C.dReal(axis[2]))
+	// TODO:  Fix this
+	panic("deprecated")
+	//C.dJointSetHinge2Axis2(j.c(), C.dReal(axis[0]), C.dReal(axis[1]), C.dReal(axis[2]))
 }
 
 // Axis2 returns the second axis.
